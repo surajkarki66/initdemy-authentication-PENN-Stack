4c5216058385cdb8368cda2164e33a70a7d5a167
surajkarki66 <suraj.karki500@protonmail.com>
2022-04-15 22:05:30 +0545

    Initial frontend setup

---
+++ client/pages/_app.tsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.css";
+
+import "../public/css/styles.css";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">Hello</h1>
+      <p>from next.js</p>
+    </>
+  );
+};
+
+export default Home;
