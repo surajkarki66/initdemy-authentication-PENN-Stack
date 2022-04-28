5bef849556b2b6d6b1bc20c668c4357f633d7396
= <suraj.karki500@protonmail.com>
2022-04-28 16:00:38 +0545

    Create register page

---
+++ client/pages/register.tsx
@@ -0,0 +1,62 @@
+import { useState, SyntheticEvent } from "react";
+import type { NextPage } from "next";
+
+const Register: NextPage = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">Register</h1>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="form-control mb-4 p-3"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            placeholder="Enter first name"
+            required
+          />
+          <input
+            type="text"
+            className="form-control mb-4 p-3"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            placeholder="Enter last name"
+            required
+          />
+          <input
+            type="email"
+            className="form-control mb-4 p-3"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email address"
+            required
+          />
+          <input
+            type="password"
+            className="form-control mb-4 p-3"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+            required
+          />
+          <br />
+          <div className="d-grid gap-2">
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
