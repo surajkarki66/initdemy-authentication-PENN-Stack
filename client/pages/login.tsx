0f86598d42bae4051489467496aad8ad777a138a
= <suraj.karki500@protonmail.com>
2022-04-28 16:01:05 +0545

    Create login page

---
+++ client/pages/login.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Login: NextPage = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">Login</h1>
+      <p>Login page...</p>
+    </>
+  );
+};
+
+export default Login;
