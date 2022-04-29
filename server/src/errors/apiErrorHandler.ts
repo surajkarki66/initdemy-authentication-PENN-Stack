3c50521a84f72d84e22ae5a9d65bec34ec45fb76
= <suraj.karki500@protonmail.com>
2022-04-29 10:30:28 +0545

    Add errors file

---
+++ server/src/errors/apiErrorHandler.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+
+import ApiError from "./apiError";
+import writeServerResponse from "../helpers/response";
+
+const apiErrorHandler = (
+  err: ApiError,
+  _req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _next: NextFunction
+) => {
+  let serverResponse = {
+    result: {
+      status: "failed",
+      data: { error: err.message },
+    },
+    statusCode: err.code,
+    contentType: "application/json",
+  };
+  if (err instanceof ApiError) {
+    return writeServerResponse(res, serverResponse);
+  }
+  serverResponse = {
+    ...serverResponse,
+    statusCode: 500,
+  };
+
+  return writeServerResponse(res, serverResponse);
+};
+
+export default apiErrorHandler;
