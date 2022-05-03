9c213c7408ef7f6790945af7adf38e7bc4bbac03
= <suraj.karki500@protonmail.com>
2022-05-03 19:06:37 +0545

    Created isUserExist and createUser function

---
+++ server/src/services/user.service.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+
+import { IRegisterUserInput } from "../interfaces/register-user-input";
+import { IUser } from "../interfaces/user";
+import { hashPassword } from "../utils/auth";
+
+const prisma = new PrismaClient();
+
+export const isUserExist = async (email: string) => {
+  try {
+    const existingUserByEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (existingUserByEmail) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createUser = async (input: IRegisterUserInput): Promise<IUser> => {
+  try {
+    const hashedPassword = String(await hashPassword(input.password));
+    const user = await prisma.user.create({
+      data: { ...input, password: hashedPassword },
+    });
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
