3489f0d60456f5789dda640aa80fa998dd3de6fd
= <suraj.karki500@protonmail.com>
2022-05-03 19:20:22 +0545

    Create user interfaces

---
+++ server/src/interfaces/register-user-input.ts
@@ -0,0 +1,6 @@
+export interface IRegisterUserInput {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+}
---
+++ server/src/interfaces/user.ts
@@ -0,0 +1,14 @@
+type UserRole = "SUBSCRIBER" | "INSTRUCTOR" | "ADMIN";
+
+export interface IUser {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  picture: string;
+  role: UserRole;
+  stripeAccountId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
