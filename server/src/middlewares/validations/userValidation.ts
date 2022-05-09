ad61893185d582d51040113fc6087832aea5697c
= <suraj.karki500@protonmail.com>
2022-05-09 22:32:01 +0545

    Deleted rememberMe

--- server/src/middlewares/validations/userValidation.ts
+++ server/src/middlewares/validations/userValidation.ts
@@ -50,10 +50,6 @@ export default function userValidation(method: string): ValidationChain[] {
           .notEmpty()
           .isString()
           .withMessage("Password must be string"),
-        body("rememberMe")
-          .default(false)
-          .isBoolean()
-          .withMessage("rememberMe must be a boolean"),
       ];
     }
 
