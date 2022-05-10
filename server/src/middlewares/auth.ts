6dba351cf12ce3b6e0e73af3e879514c8d0be5f2
= <suraj.karki500@protonmail.com>
2022-05-10 17:09:55 +0545

    Created auth middleware

---
+++ server/src/middlewares/auth.ts
@@ -0,0 +1,56 @@
+import { Request, Response, NextFunction, RequestHandler } from "express";
+
+import ApiError from "../errors/apiError";
+import config from "../configs/config";
+import { ITokenPayload } from "../helpers/types/ITokenPayload";
+import { verifyToken } from "../helpers/jwtHelper";
+
+const authenticate: RequestHandler = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  if (req.headers["authorization"]) {
+    const authorization = req.headers["authorization"].split(" ");
+    if (authorization[0] !== "Bearer") {
+      next(ApiError.unauthorized("Authentication failed."));
+      return;
+    } else {
+      try {
+        const response = await verifyToken({
+          token: authorization[1],
+          secretKey: String(config.jwtSecret),
+        });
+
+        const { id, error, role } = response as unknown as ITokenPayload;
+
+        if (id) {
+          req.user = { id: id, role };
+          return next();
+        }
+        next(ApiError.forbidden(error));
+        return;
+      } catch (error) {
+        next(ApiError.forbidden(`Token is not verified: ${error}`));
+        return;
+      }
+    }
+  } else {
+    next(ApiError.unauthorized("Authentication failed"));
+    return;
+  }
+};
+
+const permit = (roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { role } = req.user;
+    if (roles.includes(role)) {
+      next();
+    } else {
+      next(ApiError.unauthorized(`${role} is not allowed`));
+      return;
+    }
+  };
+};
+
+export { authenticate, permit };
