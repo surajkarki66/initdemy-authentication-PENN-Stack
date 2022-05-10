e558e64508e36ac3ad1df09745292d5ef7538f4c
= <suraj.karki500@protonmail.com>
2022-05-10 19:02:22 +0545

    Added and modified user services

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-import { IRegisterUserInput } from "../interfaces/register-user-input";
+import config from "../configs/config";
+import ApiError from "../errors/apiError";
 import { IUser } from "../interfaces/user";
-import { hashPassword } from "../utils/auth";
+import { comparePassword, hashPassword } from "../utils/auth";
+import { IRegisterUserInput } from "../interfaces/register-user-input";
+import { ILoginUserInput } from "../interfaces/login-user-input";
+import { NextFunction } from "express";
+import { signToken } from "../helpers/jwtHelper";
 
 const prisma = new PrismaClient();
 
@@ -27,18 +32,66 @@ export const isUserExist = async (email: string) => {
 };
 
 export const getUserByEmail = async (email: string) => {
-  return await prisma.user.findUnique({
+  return (await prisma.user.findUnique({
     where: {
       email,
     },
-  });
+  })) as IUser;
 };
-export const createUser = async (input: IRegisterUserInput): Promise<IUser> => {
+
+export const loginUser = async (
+  next: NextFunction,
+  loginInput: ILoginUserInput
+) => {
+  const { email, password } = loginInput;
+  const user = await getUserByEmail(email);
+  if (!user) {
+    next(ApiError.badRequest("Email is incorrect."));
+    return;
+  }
+  const match = await comparePassword(password, user.password);
+  if (match) {
+    const payload = {
+      id: user.id,
+      role: user.role,
+    };
+
+    const accessToken = signToken(payload, config.jwtExpires);
+    return { accessToken, user };
+  }
+  next(ApiError.badRequest("Incorrect password."));
+  return;
+};
+
+export const createUser = async (
+  next: NextFunction,
+  inputUser: IRegisterUserInput
+) => {
   try {
-    const hashedPassword = String(await hashPassword(input.password));
+    const { email } = inputUser;
+    if (await isUserExist(email)) {
+      next(ApiError.badRequest("Email already taken."));
+      return;
+    }
+    const hashedPassword = String(await hashPassword(inputUser.password));
+
     const user = await prisma.user.create({
-      data: { ...input, password: hashedPassword },
+      data: { ...inputUser, password: hashedPassword },
     });
+
+    return user as IUser;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getCurrentUser = async (id: string) => {
+  try {
+    const user = (await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })) as IUser;
 
     return user;
   } catch (error) {
