2b0da6bf63f7a848fc809272488f09bb0d65326e
= <suraj.karki500@protonmail.com>
2022-05-15 09:22:03 +0545

    Created stripe callback page

---
+++ client/pages/stripe/callback.tsx
@@ -0,0 +1,29 @@
+import { useContext, useEffect } from "react";
+import { SyncOutlined } from "@ant-design/icons";
+
+import Axios from "../../axios-url";
+import { AuthContext } from "../../context/AuthContext";
+import UserRoute from "../../components/routes/UserRoute";
+
+const StripeCallback = () => {
+  const {
+    state: { user },
+  } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      Axios.post("/instructor/get-account-status").then((_res) => {
+        window.location.href = "/instructor";
+      });
+    }
+  }, [user]);
+
+  return (
+    <SyncOutlined
+      spin
+      className="d-flex justify-content-center display-1 text-danger p-5"
+    />
+  );
+};
+
+export default StripeCallback;
